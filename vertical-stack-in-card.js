6389bf5dc25e9287c1a54ecb462379ec4afbb924
Ofek Ashery <ofek.ashery@gmail.com>
2018-07-25 19:19:55 +0300

    Support for horizontal stack and vertical stack cards

--- vertical-stack-in-card.js
+++ vertical-stack-in-card.js
@@ -5,12 +5,13 @@ class VerticalStackInCard extends HTMLElement {
         this.attachShadow({ mode: 'open' });
     }
     setConfig(config) {
-        if (!config || !config.cards  | !Array.isArray(config.cards)) {
+        if (!config || !config.cards || !Array.isArray(config.cards)) {
             throw new Error('Card config incorrect');
         }
 
         this.style.boxShadow = "0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.15)";
         this.style.borderRadius = "2px";
+        this.style.paddingBottom = "16px";
         this.style.background = "#fff";
 
         const root = this.shadowRoot;
@@ -33,40 +34,43 @@ class VerticalStackInCard extends HTMLElement {
         const config = this._config;
         const root = this.shadowRoot;
         let index = 0;
-        let stacks_index = 0;
+        let stack_index = 0;
         config.cards.forEach(item => {
-          if (item.type === "vertical-stack" || item.type === "horizontal-stack"){
-              root.childNodes[stacks_index].setConfig(item);
-              root.childNodes[stacks_index].hass = hass;
-              if (root.childNodes[stacks_index].shadowRoot) {
-                  var searchEles = root.childNodes[stacks_index].shadowRoot.getElementById("root").childNodes;
-                  for(var i = 0; i < searchEles.length; i++) {
-                      searchEles[i].style.margin = "0px";
-                      this.configCard(searchEles[i].shadowRoot.querySelector('ha-card'), index);
-                      index++;
-                  }
-                  stacks_index++;
-              }
-          } else {
-              root.childNodes[index].setConfig(item);
-              root.childNodes[index].hass = hass;
-              if (root.childNodes[index].shadowRoot) {
-                  this.configCard(root.childNodes[index].shadowRoot.querySelector('ha-card'), index)
-                  index++;
-              }
-          }
+
+            if (item.type === "vertical-stack" || item.type === "horizontal-stack"){
+                root.childNodes[stack_index].setConfig(item);
+                root.childNodes[stack_index].hass = hass;
+                stack_index++;
+            } else {
+                root.childNodes[index].setConfig(item);
+                root.childNodes[index].hass = hass;
+            }
+
+            if (root.childNodes[index].shadowRoot) {
+                if (!root.childNodes[index].shadowRoot.querySelector('ha-card')) {
+                    var searchEles = root.childNodes[index].shadowRoot.getElementById("root").childNodes;
+                    for(var i = 0; i < searchEles.length; i++) {
+                        searchEles[i].style.margin = "0px";
+                        searchEles[i].shadowRoot.querySelector('ha-card').style.boxShadow = 'none';
+                        searchEles[i].shadowRoot.querySelector('ha-card').style.paddingBottom = '0px';
+                        if (item.type != "vertical-stack") {
+                            searchEles[i].shadowRoot.querySelector('ha-card').style.paddingTop = '0px';
+                        }
+                    }
+                } else {
+                    root.childNodes[index].shadowRoot.querySelector('ha-card').style.boxShadow = 'none';
+                    root.childNodes[index].shadowRoot.querySelector('ha-card').style.paddingBottom = '0px';
+                    if(index > 0) {
+                        root.childNodes[index].shadowRoot.querySelector('ha-card').style.paddingTop = '0px';
+                    }
+                }
+            }
+            index++;
         })
     }
 
     getCardSize() {
         return 1;
-    }
-
-    configCard(card, index) {
-        card.style.boxShadow = 'none';
-        if(index > 0) {
-            card.style.paddingTop = '0px';
-        }
     }
 }
 customElements.define('vertical-stack-in-card', VerticalStackInCard);
